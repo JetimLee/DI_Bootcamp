f724e2b97fd585ece16e65a959e17b71d6d0ecad
Jetim Lee <myEmail@example.com>
2021-04-11 10:52:24 +0300

    finished mandatory exercise

---
+++ Week2/day1/mandatory_exercise/script.js
@@ -0,0 +1,29 @@
+console.log("this works");
+
+let a = 5 + "34";
+let b = 5 - "4";
+let c = 10 % 5;
+let d = 5 % 10;
+let e = "Java" + "Script";
+let f = " " + " ";
+let g = " " + 0;
+let h = true + true;
+let i = true + false;
+let j = false + true;
+let k = false - true;
+let l = 3 - 4;
+let m = "Bob" - "bill";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
