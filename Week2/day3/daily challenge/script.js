63192807816ab209b26922aa237930d425c82e62
Jetim Lee <myEmail@example.com>
2021-04-13 12:24:23 +0300

    finished daily challenge

---
+++ Week2/day3/daily challenge/script.js
@@ -0,0 +1,11 @@
+let star = "*";
+
+// for (i = 0; i < 7; i++) {
+//   console.log(star.repeat(i));
+// }
+
+for (i = 1; i < 7; i++) {
+  for (x = 0; x < 1; x++) {
+    console.log(star.repeat(i));
+  }
+}
