dcda4818cfaff0ca0e6051968ede0d3dc96ff7f9
Jetim Lee <myEmail@example.com>
2021-04-13 15:15:46 +0300

    misunderstood exercise 4, re-did it.

--- Week2/day3/mandatory exercises/exercise 4/script.js
+++ Week2/day3/mandatory exercises/exercise 4/script.js
@@ -8,11 +8,17 @@ let guestList = {
 
 let userName = prompt(`Please enter your name`);
 
-for (const property in guestList) {
-  if (userName == property) {
-    console.log(`Hello, I am ${property} and I am from ${guestList[property]}`);
-    break;
-  } else if (userName != property) {
-    console.log(`I am a guest!`);
-  }
+if (userName in guestList) {
+  console.log(`Hi, I am ${userName} and I am from ${guestList[userName]}`);
+} else {
+  console.log(`i am a guest`);
 }
+
+// for (const property in guestList) {
+//   if (userName == property) {
+//     console.log(`Hello, I am ${property} and I am from ${guestList[property]}`);
+//     break;
+//   } else {
+//     console.log(`I am a guest!`);
+//   }
+// }
