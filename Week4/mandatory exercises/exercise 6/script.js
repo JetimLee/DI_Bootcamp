7930799c17db9de1fcbcfe32be56ac434ae6a49e
gavin <gavincoulson1@gmail.com>
2021-04-25 17:54:39 +0300

    finished mandatory exercises

--- Week4/mandatory exercises/exercise 6/script.js
+++ Week4/mandatory exercises/exercise 6/script.js
@@ -6,3 +6,33 @@
 // Example : let details = ["+200", "-100", "+146", "+167", "-2900"]
 // Create a program that modifies the expenses so that they will include taxes (multiply each expense by the VAT).
 // Display your current bankAccount standing at the end of the month.
+
+let bankAmount = 50000;
+
+let moneyStuff = ["+200", "-300", "+8", "+100"];
+
+const getEndOfMonth = (arr) => {
+  let vat = 0.17;
+  let expenses;
+  let negativeIncrease;
+  let newLoss;
+  let taxes;
+  let newExpenses;
+  for (let i = 0; i < arr.length; i++) {
+    expenses = parseInt(arr[i]);
+    if (expenses < 0) {
+      negativeIncrease = expenses * -vat;
+      newLoss = negativeIncrease - arr[i];
+      console.log(newLoss);
+    } else {
+      taxes = expenses * vat;
+      newExpenses = expenses - taxes;
+      console.log(newExpenses);
+    }
+    taxes = expenses * vat;
+    newExpenses = expenses - taxes;
+  }
+  alert(`Your new balance is ${bankAmount + newExpenses - newLoss}`);
+};
+
+getEndOfMonth(moneyStuff);
