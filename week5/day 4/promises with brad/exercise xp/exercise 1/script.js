e671f6422269a11055e919a85c28a3ac866497a3
gavin <gavincoulson1@gmail.com>
2021-05-05 11:28:35 +0300

    fixed exercise 1

--- week5/day 4/promises with brad/exercise xp/exercise 1/script.js
+++ week5/day 4/promises with brad/exercise xp/exercise 1/script.js
@@ -1,19 +1,43 @@
 let numbers = /\d/g;
 
 const compareToTen = (num) => {
-  setTimeout(() => {
-    let numberCheck = new Promise((resolve, reject) => {
-      if (numbers.test(num) == true && num > 10) {
-        console.log(`${num} is a number greater than 10`);
+  let numberCheck = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (numbers.test(num) == true && num >= 10) {
+        resolve(`${num} is a number greater than 10`);
       } else if (num < 10) {
         reject(`${num} is less than 10, error`);
       } else if (numbers.test(num) != true) {
         reject(`${num} isn't a number!`);
       }
-    });
-  }, 2000);
+    }, 2000);
+  });
+  return numberCheck;
 };
 
-console.log(compareToTen(11));
-console.log(compareToTen("a"));
-console.log(compareToTen(9));
+const compareToTenn = (num) => {
+  let p = new Promise((resolve, reject) => {
+    if (num > 10) {
+      resolve("bal bla bla");
+    } else {
+      reject("ha ha ha");
+    }
+  });
+  return p;
+};
+
+compareToTen(11)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+compareToTenn(11)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
