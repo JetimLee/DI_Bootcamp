45b29b614bceb6cd6335f0dab2e3e5e1ccdc9479
gavin <gavincoulson1@gmail.com>
2021-05-06 16:43:01 +0300

    finished

--- week5/day 5/mini project star wars/script.js
+++ week5/day 5/mini project star wars/script.js
@@ -3,20 +3,37 @@ const heightArea = document.getElementById("height");
 const birthYear = document.getElementById("birthYear");
 const homeWorld = document.getElementById("homeWorld");
 const button = document.getElementById("btn");
+const container = document.getElementById("textContainer");
+const loader = document.getElementById("loader");
+const loadingP = document.createElement("p");
+loadingP.classList.add("loading");
 
 const generateRandomInformation = (obj) => {
+  loader.style.display = "none";
+
   nameArea.innerText = obj.name;
   heightArea.innerText = obj.height;
   birthYear.innerText = obj.birth_year;
+  container.style.backgroundColor = "black";
+  loadingP.style.display = "none";
 };
 
 const homeWorldName = async (obj) => {
-  console.log(obj.homeworld);
-  let response = await fetch(obj.homeworld).then((response) => response.json());
+  let response = await fetch(obj.homeworld)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+    });
   homeWorld.innerText = response.name;
 };
 
 const updateInformation = async () => {
+  loadingP.innerText = "LOADING!";
+  container.innerHTML.display = "none";
+  loader.style.display = "block";
+  container.style.backgroundColor = "#2B7B8C";
+  container.prepend(loadingP);
+  loadingP.innerText = "LOADING!";
   let randomNumber = Math.floor(Math.random() * 10 + 1);
   let url = `https://swapi.dev/api/people/${randomNumber}/`;
   let response = await fetch(url)
@@ -32,7 +49,7 @@ const getStarWars = async () => {
   let randomNumber = Math.floor(Math.random() * 10 + 1);
   let url = `https://swapi.dev/api/people/${randomNumber}/`;
   let response = await fetch(url);
-  console.log(`loading`);
+
   return response.json();
 };
 
@@ -42,8 +59,17 @@ getStarWars()
 
     generateRandomInformation(results);
     homeWorldName(results);
-    console.log(results);
   })
   .catch((error) => {
     console.log(`an error occurred ${error}`);
   });
+
+const load = () => {
+  container.innerHTML.display = "hidden";
+  loader.style.display = "block";
+  container.style.backgroundColor = "#2B7B8C";
+  container.prepend(loadingP);
+  loadingP.innerText = "LOADING!";
+};
+
+window.addEventListener("load", load);
