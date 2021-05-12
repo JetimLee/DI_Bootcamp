f8e276d478b020b73d60ca84585f021064f133e3
gavin <gavincoulson1@gmail.com>
2021-05-12 20:45:30 +0300

    finished maybe?

---
+++ Re-dos/promise all/script.js
@@ -0,0 +1,15 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
+});
+
+let arrayOfPromises = [promise1, promise2, promise3];
+
+const doThePromiseThing = (array) => {
+  Promise.all([array])
+    .then((values) => {
+      console.log(values);
+    })
+    .catch((error) => console.log(error));
+};
