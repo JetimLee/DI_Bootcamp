0ef5355ebc3ac1fe619bb7609626cf6079f82435
gavin <gavincoulson1@gmail.com>
2021-05-18 18:06:45 +0300

    updated some more stuff

--- updated_portfolio/src/components/intro/Intro.jsx
+++ updated_portfolio/src/components/intro/Intro.jsx
@@ -1,5 +1,5 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro">yalla</div>;
+  return <div className="intro" id="intro"></div>;
 }
--- updated_portfolio/src/components/topbar/Topbar.jsx
+++ updated_portfolio/src/components/topbar/Topbar.jsx
@@ -1,4 +1,15 @@
 import "./topbar.scss";
 export default function Topbar() {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo" alt="intros">
+            intro
+          </a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
