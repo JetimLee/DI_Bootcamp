f3c307089b72b1e83055bc2acc12d583633c074d
gavin <gavincoulson1@gmail.com>
2021-05-24 18:50:32 +0300

    added a minor line

--- week8nodeJS/day 2 class/daily_challenge/script.js
+++ week8nodeJS/day 2 class/daily_challenge/script.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.get("/", (request, response) => {
 //   response.send("hello world");
