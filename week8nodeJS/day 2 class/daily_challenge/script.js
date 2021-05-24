35c31a38a24b1fc29e9589faa125fb16883c404d
gavin <gavincoulson1@gmail.com>
2021-05-24 18:38:36 +0300

    maybe i finished daily?

---
+++ week8nodeJS/day 2 class/daily_challenge/script.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+// app.get("/", (request, response) => {
+//   response.send("hello world");
+// });
+
+app.get("/aboutme/:hobby", (req, res) => {
+  res.send("kung fu");
+});
+app.get("*", (req, res) => {
+  //star represents all the wrong routes
+  res.send("404 page");
+});
+
+app.post("/form", function (req, res) {
+  console.log(req.body);
+  res.send("recieved your request!");
+});
+
+app.listen(3004, () => {
+  console.log("server is listening on port 3004");
+});
