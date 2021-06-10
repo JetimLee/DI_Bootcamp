de17576322242f9d0f64befe08ef01291916864b
gavin <gavincoulson1@gmail.com>
2021-06-10 18:42:23 +0300

    will review this some more

---
+++ week_9_react/Autocomplete_form/autocomplete-form/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import AutoCompletedText from "./components/AutoCompletedText";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <AutoCompletedText />
+      </div>
+    );
+  }
+}
