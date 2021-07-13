93e3faf72033fa853912dff671b33ef259e40f12
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-13 18:51:06 +0300

    learned how variables work in python

--- pythonlearning/test.py
+++ pythonlearning/test.py
@@ -7,6 +7,19 @@ print(2*4)
 print(2/4)
 print(type(2.3))
 print(type(2))
+
+print(2**3)
+print(2//4)
+print(5//4) #this will rown down to an integer
+print(5%4)#what will be left of this division will be the result
+
+#math functions
+
+print(round(3.1))
+print(round(3.5))
+print(abs(-20)) #returns absolute value of the argument - no negative numbers
+
+#operator precedence, Please Excuse My Dear Aunt Sally
 float #a float takes up a lot more space in memory than an integer. a float is anything with a decimal in it
 bool #boolean true/false
 str #all letters
@@ -14,7 +27,11 @@ list
 tuple
 set
 dict
-
+complex #a complex number is a 3rd type of number - this is for really really complex math
+bin(5) #will return whatever the binary version of 5 is
+print(bin(5)) #when python sees 0b, python knows it's a binary number
+#can convert it
+print(int('0b101',2)) #binary numbers are always a base 2 number, so the conversion is being done here
 #classes --> custom types
 
 #specialized data types 
---
+++ pythonlearning/variables.py
@@ -0,0 +1,8 @@
+q = 190
+#how variables work in python
+print(q)
+
+a,b,c=1,2,3
+print(a,b,c)
+#can override variables easily, so try to avoid doing that
+snake_case=180
