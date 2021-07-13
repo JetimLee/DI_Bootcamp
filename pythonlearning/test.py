cee44ba3243464ccd11f69ea8343e4d945b3900e
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-13 17:31:50 +0300

    started learning python today

---
+++ pythonlearning/test.py
@@ -0,0 +1,22 @@
+#Fundamental data types
+int #number
+3,4,5,6
+2+4
+print(2+4)
+print(2*4)
+print(2/4)
+print(type(2.3))
+print(type(2))
+float #a float takes up a lot more space in memory than an integer. a float is anything with a decimal in it
+bool #boolean true/false
+str #all letters
+list
+tuple
+set
+dict
+
+#classes --> custom types
+
+#specialized data types 
+
+None #means nothing - abscence of value
