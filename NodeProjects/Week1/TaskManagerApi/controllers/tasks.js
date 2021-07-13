55f0ddb3b645c81406c4fa012d34986ebf71b903
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-13 11:00:05 +0300

    can now delete tasks

--- NodeProjects/Week1/TaskManagerApi/controllers/tasks.js
+++ NodeProjects/Week1/TaskManagerApi/controllers/tasks.js
@@ -1,22 +1,56 @@
 const Task = require("../models/task");
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  let tasks;
+  try {
+    tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).send(`There was an error in creating the task`);
+  }
 };
 
 const createTask = async (req, res) => {
   const { name, completed } = req.body;
-  const task = await Task.create({ name, completed });
-  res.status(201).json(task);
+  let task;
+  try {
+    task = await Task.create({ name, completed });
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).send(`There was an error in creating the task`);
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  let task;
+  try {
+    task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      res.status(404).json({ msg: `No task found with that ID` });
+      return;
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).send(`there was an error in finding the task!`);
+  }
 };
 const editTask = (req, res) => {
   res.send("edit task");
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  let task;
+  try {
+    task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      res.status(404).json({ msg: `No task found with that ID` });
+    } else {
+      await task.delete();
+      res.status(200).json({ msg: `Task with id of ${taskID} deleted` });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 //in an object here because i will be exporting multiple routes, helps keep code DRY
 module.exports = {
