d688d8021689ce062189dead022ebf5ab35853e9
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-15 10:12:31 +0300

    did type conversion practice

---
+++ pythonlearning/week1/Booleans.py
@@ -0,0 +1,7 @@
+#booleans
+bool #can be true or false
+
+name = 'gavin'
+is_cool = True
+
+print(f'{name} {is_cool}')
---
+++ pythonlearning/week1/typeConversion.py
@@ -0,0 +1,4 @@
+birth_year = input('what year were you born?')
+current_year = 2021
+
+print(f'Your age is {int(current_year) - int(birth_year)}')
