91855b0dda5267a05d010f335f6cd25d3be977f0
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-15 15:40:53 +0300

    did a password checker

---
+++ pythonlearning/week1/passwordChecker.py
@@ -0,0 +1,5 @@
+username = input('Please enter a username: ')
+password = input('Please enter your password: ')
+passwordLength = len(password) * '*'
+
+print(f'Hello {username} your password of {passwordLength} is {len(password)}')
