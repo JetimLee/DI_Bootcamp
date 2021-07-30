3e065374fafc219932265857f2f2c3f7d6238120
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-07-30 16:07:53 +0300

    did some event work and made a basic counter

--- Node-ZTM-Course/week2/expressAPI/index.js
+++ Node-ZTM-Course/week2/expressAPI/index.js
@@ -23,8 +23,28 @@ const friends = [
   },
 ];
 
+app.use((req, resp, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+//parses the data from clientside
+app.use(express.json());
+
 app.get("/", (req, resp) => {
   resp.json(friends);
+});
+app.post("/", (req, resp) => {
+  const { name } = req.body;
+  if (!name) {
+    resp.status(400).json({ message: "must enter a name!" });
+  } else {
+    const newFriend = {
+      name,
+      id: friends.length,
+    };
+    friends.push(newFriend);
+    resp.status(200).json(newFriend);
+  }
 });
 
 app.get("/friend/:friendId", (req, resp) => {
